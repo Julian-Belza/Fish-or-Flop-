73dd225ea8f46082a2db997135124822785ab84b
Derean Simmons <dereransim1@gmail.com>
2024-04-09 08:23:10 -0400

    interacting objects script

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Interactible : MonoBehaviour
+public class Interactable : MonoBehaviour
 {
     Outline outline;
     public string message;
@@ -15,12 +15,6 @@ public class Interactible : MonoBehaviour
     {
         outline = GetComponent<Outline>();
         DisableOutline();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     public void Interact()
