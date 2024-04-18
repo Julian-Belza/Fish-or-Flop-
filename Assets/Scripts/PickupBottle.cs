6d5cc6f2d7581618f06910f0eb3c445697d8e66c
Derean Simmons <dereransim1@gmail.com>
2024-04-18 09:14:06 -0400

    place down bottle script

--- Assets/Scripts/PickupBottle.cs
+++ Assets/Scripts/PickupBottle.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PickupBottle : MonoBehaviour
 {
+    Outline outline;
+    public GameObject playerBottle;
+    public GameObject pickupBotText;
+    public bool hasBottle;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hasBottle = false;
+        playerBottle.SetActive(false);
+        pickupBotText.SetActive(false);
+        outline = GetComponent<Outline>();
+        DisableOutline();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay(Collider other)
     {
-        
+        if (other.gameObject.tag == "Player" && !hasBottle)
+        {
+            EnableOutline();
+            pickupBotText.SetActive(true);
+            if (Input.GetKey(KeyCode.E))
+            {
+                pickupBotText.SetActive(false);
+                hasBottle = true;
+                this.gameObject.SetActive(false);
+                playerBottle.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        DisableOutline();
+        pickupBotText.SetActive(false);
+    }
+
+    public void DisableOutline()
+    {
+        outline.enabled = false;
+    }
+
+    public void EnableOutline()
+    {
+        outline.enabled = true;
     }
 }
