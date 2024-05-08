2e115c7d49361612ae777c19dd9ca09595b5a8ad
Derean Simmons <dereransim1@gmail.com>
2024-05-08 07:43:35 -0400

    editing inventory text

--- Assets/Scripts/OpenInventory.cs
+++ Assets/Scripts/OpenInventory.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class OpenInventory : MonoBehaviour
 {
     public GameObject inventory;
     bool inventoryOpen;
+    public TMP_Text txt;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,11 +23,13 @@ public class OpenInventory : MonoBehaviour
             if (!inventoryOpen)
             {
                 inventory.SetActive(true);
+                txt.SetText("Press TAB to close");
                 inventoryOpen = true;
             }
             else
             {
                 inventory.SetActive(false);
+                txt.SetText("Press TAB to open inventory");
                 inventoryOpen = false;
             }
         }
